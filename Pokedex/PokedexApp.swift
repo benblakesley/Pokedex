12f6b6c5254fd17dd673de0a031b01d89abd6e23
BenBlakesley <benblakesley9@gmail.com>
2024-10-08 19:28:53 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ben Blakesley on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
